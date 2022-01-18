66d7411374f3d5a8d85fdd489e11ff28bb43384e
sourav <samantasourav184@gamil.com>
2022-01-19 01:22:47 +0530

    added youtube download

--- app.js
+++ app.js
@@ -13,7 +13,29 @@ App.get('/', (req, res) => {
         data: null
     })
 })
+
 App.use("/api", Api)
+App.get('/ytdownload', async (req, res) => {
+    let videoId = req.query.videoid
+    // let url = "https://www.youtube.com/watch?v=" + videoId
+    let url = req.query.url
+    let format = req.query.format || "mp4"
+    let quality = req.query.quality || "highest"
+    format = format.toLowerCase()
+    quality = quality.toLowerCase()
+
+    try {
+        Youtube(url, {
+            format: format,
+            quality: quality
+        }).pipe(res)
+    } catch (err) {
+        console.log(err)
+        Youtube(url, {
+            format: "mp4"
+        }).pipe(res)
+    }
+})
 
 App.listen(port, () => {
     console.log(`Express app running on port ${port}`)
